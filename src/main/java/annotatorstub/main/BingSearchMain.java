ce0e30051ef5829b4f48bce381eaca4cc1fed50d
Yeyao Zhang <530066739@qq.com>
2016-05-04 10:55:18 +0200

    add a new method getBingSearchResult() pass in a query containing the target mention return a concatenation of the top 3 results returned by Bing Search Engine, as an expanded representation of the original query.

--- src/main/java/annotatorstub/main/BingSearchMain.java
+++ src/main/java/annotatorstub/main/BingSearchMain.java
@@ -1,16 +1,29 @@
 package annotatorstub.main;
 
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONObject;
 
 import it.unipi.di.acube.BingInterface;
 
 public class BingSearchMain {
-	public static void main(String[] args) throws Exception {
-		BingInterface bing = new BingInterface("<your key goes here>");
-		JSONObject a = bing.queryBing("funny kittens");
-
-		// see: http://datamarket.azure.com/dataset/bing/search#schema for
-		// query/response format
-		System.out.println(a.getJSONObject("d").getJSONArray("results").getJSONObject(0).toString(4));
+	final int num_top_result = 3; // the number of returned results that we consider
+	/**
+	 * Given a query, return the concatenation of the top 3 bing search result(title + description per result).
+	 * @param query The query containing the mention
+	 * @return A String value representing the concatenation of the top 3 result.
+	 * @throws Exception
+	 */
+	String getBingSearchResult(String query) throws Exception{
+		BingInterface bing = new BingInterface("IRvN9mc0Zql0YWe30+gGlHtF7/uQc1WJ8YBiy/HuLiI");
+		JSONObject a = bing.queryBing(query);
+		JSONArray res_arr = a.getJSONObject("d").getJSONArray("results").getJSONObject(0).getJSONArray("Web");
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < num_top_result; i ++){
+			JSONObject obj = res_arr.getJSONObject(i);
+			sb.append(obj.getString("Title"));
+			sb.append(obj.getString("Description"));
+		}
+		return sb.toString();		
 	}
+
 }
